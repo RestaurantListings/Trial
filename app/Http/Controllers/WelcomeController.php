6971b86c1ee6f0ac3d0daebd7710e36b844cf557
abu-actsinfo <abu.actsinfo@gmail.com>
2016-01-07 11:13:48 +0530

    homepage updated

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -60,8 +60,7 @@ class WelcomeController extends Controller {
             ->take(4)
             ->get();
 
-        $data['recent_reviews'] = \App\Restaurant_Reviews::where('source', 'LIKE', '%GOOGLE%')->orderBy(DB::raw('RAND()'))
-            ->where('restaurants.city_id', '=', $city)
+        $data['recent_reviews'] = \App\Restaurant_Reviews::orderBy(DB::raw('RAND()'))
             ->leftJoin('restaurants', 'restaurants_reviews.restaurants_id', '=', 'restaurants.id')
             ->leftJoin('city', 'restaurants.city_id', '=', 'city.id')
             ->leftJoin('state', 'restaurants.state_id', '=', 'state.id')
@@ -70,14 +69,12 @@ class WelcomeController extends Controller {
         //$data['nearest_zip'] = \App\Zip::where('zip', '>', (int)session('geoip-locations.postal_code')-10)->where('zip', '<', (int)session('geoip-locations.postal_code')+10)
           //  ->take(5)->get();
         //dd(DB::getQueryLog());
+        //dd($data['recent_reviews']);
 
         if($agent->isMobile()){
             return view('mobile_home')->with($data);
         }else{
             return view('home')->with($data);
         }
-
-
     }
-
 }
